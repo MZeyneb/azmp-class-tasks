b2682e6b0085275dfd5e17f364535f09de4a7feb
mzeyneb <zeynebmustafa05@gmail.com>
2024-12-24 19:03:55 +0400

    site

---
+++ routing/src/pages/Client/Contact/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+const index = () => {
+  return (
+    <div className="hero-container">
+    <h1 className="hero-title">Contacts</h1>
+    <p className="hero-description">Welcome to the Contact page! Discover our latest updates and explore our offerings.</p>
+  </div>
+  )
+}
+
+export default index
---
+++ routing/src/pages/Client/Products/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+const index = () => {
+  return (
+    <div className="hero-container">
+    <h1 className="hero-title">Products</h1>
+    <p className="hero-description">Welcome to the Products! Discover our latest updates and explore our offerings.</p>
+  </div>
+  )
+}
+
+export default index
